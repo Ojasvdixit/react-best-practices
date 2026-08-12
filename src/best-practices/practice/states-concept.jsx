0884ccccdcc7ca03eb50b31aa1f3b05938e60146
Ojasvdixit <ojasvdixit1@gmail.com>
2026-08-12 22:31:33 +0530

    key in ist logic also implemented

--- src/best-practices/practice/states-concept.jsx
+++ src/best-practices/practice/states-concept.jsx
@@ -1,21 +1,52 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const StatesConcept = () => {
 
+
   const [count,setCount]= useState(0);
+  const items= [
+    {
+      id:1,
+      name:'Rahul'
+    },
+     {
+      id:2,
+      name:'Sanskar'
+    }, {
+      id:3,
+      name:'Ojasv'
+    },
+  ]
 
   function handleIncrement(){
     setCount(count=>count+1);
     setCount(count=>count+1);
     setCount(count=>count+1);
-    setCount(count=>count+1);
 
   }
-  console.log('count',count)
+  //  if(count>0){
+  //   console.log('count',count)
+  //  }
+  //  else{
+  //   useEffect(()=>{
+  //     console.log('useEffect count',count)
+  //   },[])
+  //  }
+
 
   return (
     <div>
       <button onClick={handleIncrement}>Count {count}</button>
+      {
+        items.map((item,index)=>{
+          return (
+            <div key={item.id}>
+            {item.name}
+             <input></input>
+          </div>
+          )
+        })
+      }
     </div>
   )
 }
